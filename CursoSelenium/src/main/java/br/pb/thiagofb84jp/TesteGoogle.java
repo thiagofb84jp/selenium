4123c8780f0a96a057976b9f1ef4445a328864dc
Thiago Ferreira <thiagobarbosa@EU3146022019005.local>
2023-08-10 10:32:19 -0300

    First commit

---
+++ CursoSelenium/src/main/java/br/pb/thiagofb84jp/TesteGoogle.java
@@ -0,0 +1,35 @@
+package br.pb.thiagofb84jp;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.net.http.WebSocket;
+import java.sql.Driver;
+
+public class TesteGoogle {
+    private WebDriver driver;
+
+    @Before
+    public void setup() {
+//        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testeGoogle() {
+        Assert.assertEquals("Google", driver.getTitle());
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.close();
+    }
+}
